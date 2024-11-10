39f34d0b810b6a8ef043755228b9182c4bde87cc
MargheritaMottana <margherita.mottana@gmail.com>
2024-11-10 12:14:43 +0100

    - Suddivisa la chiamata API in pagine (3 progetti a pagina) - Aggiunto eager loading per vedere anche le relazioni con le altre tabelle

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -13,7 +13,8 @@ class ProjectController extends Controller
     public function index()
     {
 
-        $projects = Project::get();
+        // $projects = Project::get();
+        $projects = Project::with('type', 'technologies')->paginate(3);
 
         return response()->json([
             'success' => true,
